ef00d8f7aefd2acf135e62be20a349b1f176cb1a
Lyon <lyon.yang@qq.com>
2019-03-13 16:37:33 +0800

    fix markdown pictrue

--- base/settings.py
+++ base/settings.py
@@ -36,9 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'docs.apps.DocsConfig',
     'apps.articles',
-    'docs',
 ]
 
 MIDDLEWARE = [
--- docs/apps.py
+++ docs/apps.py
@@ -8,14 +8,3 @@ from django.apps import AppConfig
 class DocsConfig(AppConfig):
     name = 'docs'
     verbose_name = 'django_api_docs'
-
-    def ready(self):
-        from django.conf import settings
-        from docs import settings as docs_settings
-        def setter_settings(name):
-            if not hasattr(settings, name):
-                default_settings = getattr(docs_settings, name)
-                setattr(settings, name, default_settings)
-
-        for i in dir(docs_settings):
-            setter_settings(i)
