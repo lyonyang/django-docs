71d3ac1f0990599a0a09d1509b8782994557884d
Lyon <lyon.yang@qq.com>
2019-08-27 11:09:48 +0800

    update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 from setuptools import setup
 
-version = '2.1.2'
+version = '2.1.3'
 
 # Hack to prevent stupid TypeError: 'NoneType' object is not callable error on
 # exit of python setup.py test # in multiprocessing/util.py _exit_function when
